e21bbf12b7f6f6d5210aa814be2b51803a9f3ecf
bryynnn <161508666+BrayanCharlesEtec@users.noreply.github.com>
2026-02-10 17:27:57 -0300

    Add files via upload

---
+++ aula 1/exemplo_var.php
@@ -0,0 +1,10 @@
+<?php
+$nome = "kevin pinto grossman";
+$idade = 2;
+$preco = 676767;
+$ativo = true;
+echo "Nome: " . $nome . "<br>";
+echo "Idade: " . $idade . "<br>";
+echo "Preço: R$ " . $preco . "<br>";
+echo "Status: " . ($ativo ? "Ativo" : "Inativo");
+?>
